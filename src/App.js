f1726b6221fc942745a6e3fd69443e7c4ef34f40
huy-xu <nhathuyhedspi@gmail.com>
2021-06-05 09:42:51 +0700

    Initial project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
